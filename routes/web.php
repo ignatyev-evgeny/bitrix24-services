b8e89fa42f1b16c3bf638c83fe4f083cf76d21a6
Evgeny Ignatyev <work@ignatyev.pro>
2024-04-10 01:11:22 +0300

    Added new routes for Bitrix quiz services in web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
+use App\Http\Controllers\Services\BitrixController;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix('services')->name('services.')->group(function () {
+    Route::prefix('bitrix')->name('bitrix.')->group(function () {
+        Route::prefix('quiz')->name('quiz.')->group(function () {
+            Route::get('/home', [BitrixController::class, 'home'])->name('home');
+            Route::post('/install', [BitrixController::class, 'install'])->withoutMiddleware([VerifyCsrfToken::class])->name('install');
+        });
+    });
 });
