4cc07a55a8d9abb68756d4c574c7e0dc1dc2cc3b
Evgeny Ignatyev <work@ignatyev.pro>
2024-04-23 17:38:30 +0200

    Обновление боковой панели

--- resources/views/certification/chunk/sidebar.blade.php
+++ resources/views/certification/chunk/sidebar.blade.php
@@ -2,40 +2,40 @@
     <div class="sidebar">
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="image">
-                <img src="{!! Auth::user()->photo !!}" class="img-circle elevation-2" alt="{!! Auth::user()->name !!} {!! Auth::user()->lastname !!}">
+                <img src="{!! $auth->photo !!}" class="img-circle elevation-2" alt="{!! $auth->name !!}">
             </div>
             <div class="info">
-                <a href="#" class="d-block">{!! Auth::user()->name !!} {!! Auth::user()->lastname !!}</a>
+                <a href="#" class="d-block">{!! $auth->name !!}</a>
             </div>
         </div>
         <nav class="mt-2">
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
                 <li class="nav-item">
-                    <a href="{{ route('home', ['member_id' => Auth::user()->member_id]) }}" class="nav-link @if(Route::getCurrentRoute()->getName() == 'home') active @endif">
+                    <a href="{{ route('home', ['member_id' => $auth->member_id]) }}" class="nav-link @if(Route::getCurrentRoute()->getName() == 'home') active @endif">
                         <i class="nav-icon fas fa-chart-pie"></i>
                         <p>{{ __('Главная') }}</p>
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a href="{{ route('get.departments', ['member_id' => Auth::user()->member_id]) }}" class="nav-link @if(Route::getCurrentRoute()->getName() == 'get.departments') active @endif">
+                    <a href="{{ route('get.departments', ['member_id' => $auth->member_id]) }}" class="nav-link @if(Route::getCurrentRoute()->getName() == 'get.departments') active @endif">
                         <i class="nav-icon fas fa-users-cog"></i>
                         <p>{{ __('Подразделения') }}</p>
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a href="{{ route('get.users', ['member_id' => Auth::user()->member_id]) }}" class="nav-link @if(Route::getCurrentRoute()->getName() == 'get.users') active @endif">
+                    <a href="{{ route('get.users', ['member_id' => $auth->member_id]) }}" class="nav-link @if(Route::getCurrentRoute()->getName() == 'get.users') active @endif">
                         <i class="nav-icon fas fa-users"></i>
                         <p>{{ __('Сотрудники') }}</p>
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a href="{{ route('get.tests', ['member_id' => Auth::user()->member_id]) }}" class="nav-link @if(Route::getCurrentRoute()->getName() == 'get.tests') active @endif">
+                    <a href="{{ route('get.tests', ['member_id' => $auth->member_id]) }}" class="nav-link @if(Route::getCurrentRoute()->getName() == 'get.tests') active @endif">
                         <i class="nav-icon fas fa-edit"></i>
                         <p>{{ __('Тесты') }}</p>
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a href="{{ route('get.сertifications', ['member_id' => Auth::user()->member_id]) }}" class="nav-link @if(Route::getCurrentRoute()->getName() == 'get.сertifications') active @endif">
+                    <a href="{{ route('get.сertifications', ['member_id' => $auth->member_id]) }}" class="nav-link @if(Route::getCurrentRoute()->getName() == 'get.сertifications') active @endif">
                         <i class="nav-icon fas fa-stamp"></i>
                         <p>{{ __('Аттестации') }}</p>
                     </a>
