e5121ce79aa8f9ffb1399ffec008c75a1b3c8ac9
Evgeny Ignatyev <work@ignatyev.pro>
2024-04-23 17:38:55 +0200

    Обновление модели работы с пользователем

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,10 +3,16 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Models\Bitrix\DepartmentsModel;
+use App\Models\Bitrix\PortalsModel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
+/**
+ * @method static whereBitrixId(mixed $ID)
+ */
 class User extends Authenticatable
 {
     use HasFactory, Notifiable;
@@ -18,27 +24,20 @@ class User extends Authenticatable
         'bitrix_id',
         'active',
         'is_admin',
-        'is_administrator',
+        'is_support',
         'is_manager',
         'active',
         'name',
-        'lastname',
         'photo',
         'email',
-        'last_login',
-        'date_register',
-        'is_online',
-        'time_zone_offset',
-        'timestamp_x',
-        'last_activity_date',
-        'personal_gender',
-        'personal_birthday',
-        'user_type',
-        'uf_department',
+        'phone_personal',
+        'phone_work',
+        'departments',
+        'position',
         'lang',
         'portal',
         'auth',
-        'member',
+        'user',
         'password',
     ];
 
@@ -48,13 +47,51 @@ class User extends Authenticatable
 
     protected function casts(): array {
         return [
-            'uf_department' => 'array',
+            'departments' => 'array',
             'auth' => 'array',
-            'member' => 'array',
+            'user' => 'array',
             'password' => 'hashed',
+            'active' => 'boolean',
             'is_admin' => 'boolean',
-            'is_administrator' => 'boolean',
+            'is_support' => 'boolean',
             'is_manager' => 'boolean',
         ];
+    }
+
+    public function portalObject(): HasOne {
+        return $this->hasOne(PortalsModel::class, 'id', 'portal');
+    }
+
+    public static function getAllCount(): int {
+        return User::count();
+    }
+
+    public function departments(): array {
+        $departmentsArray = [];
+        foreach ($this->departments as $department) {
+            $departmentObject = DepartmentsModel::where('portal', $this->portal)->where('bitrix_id', $department)->first();
+            $departmentsArray[] = $departmentObject->name;
+        }
+        return $departmentsArray;
+    }
+
+    public function getEmailFormatAttribute(): ?string {
+        return !empty($this->email) ? "<a href='mailto:$this->email'>$this->email</a>" : "<b>Не указан</b>";
+    }
+
+    public function getPhotoFormatAttribute(): ?string {
+        return !empty($this->photo) ? $this->photo : asset('/img/default-150x150.png');
+    }
+
+    public function getIntActiveAttribute(): ?string {
+        return $this->active ? 1 : 0;
+    }
+
+    public function getIntIsSupportAttribute(): ?string {
+        return $this->is_support ? 1 : 0;
+    }
+
+    public function getBitrixProfileLinkAttribute(): ?string {
+        return "<a target='_blank' href='https://{$this->portalObject->domain}/company/personal/user/{$this->bitrix_id}/'>$this->bitrix_id</a>";
     }
 }
