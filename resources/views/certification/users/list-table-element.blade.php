7dfe5f76d295351b4c60adaeac4ecde609a3b3d6
Evgeny Ignatyev <work@ignatyev.pro>
2024-04-23 19:25:40 +0200

    Обновление шаблонов отображения пользователей

--- resources/views/certification/users/list-table-element.blade.php
+++ resources/views/certification/users/list-table-element.blade.php
@@ -2,7 +2,7 @@
     <td class="text-center align-middle">{{ $user->id }}</td>
     <td class="text-center align-middle">{!! $user->bitrix_profile_link !!}</td>
     <td class="text-center align-middle">
-        <input type="checkbox" name="my-checkbox" data-switch @if($user['active']) checked data-status="true" @else data-status="false" @endif data-off-color="danger" data-on-color="success">
+        <input type="checkbox" name="active" onchange="changeActiveStatus({{ $user->id }}, {{ $user->int_active }})" data-switch @if($user['active']) checked data-status="true" @else data-status="false" @endif data-off-color="danger" data-on-color="success">
     </td>
     <td class="text-left align-middle">
         <img src="{!! $user->photo_format !!}" alt="{{ $user['name'] }} {{ $user['lastname'] }}" class="img-circle img-size-32 mr-2">
@@ -10,14 +10,10 @@
     </td>
     <td class="text-center align-middle">{!! $user->email_format !!}</td>
     <td class="text-center align-middle">
-        <input type="checkbox" name="my-checkbox" data-switch @if($user['is_administrator']) checked data-status="true" @else data-status="false" @endif data-off-color="danger" data-on-color="success">
+        <input type="checkbox" name="is_support" onchange="changeIsSupport({{ $user->id }}, {{ $user->int_is_support }})" data-switch @if($user['is_support']) checked data-status="true" @else data-status="false" @endif data-off-color="danger" data-on-color="success">
     </td>
     <td class="text-center align-middle">{!! implode("<br>",$user->departments()) !!}</td>
-    <td class="text-center align-middle">{{ $user->lang ?? __('Не указан') }}</td>
+    <td class="text-center align-middle">
+        <b>{{ $user->lang ?? __('Не указан') }}</b>
+    </td>
 </tr>
-
-<script>
-    $("input[data-switch]").each(function(){
-        $(this).bootstrapSwitch('state', $(this).prop('checked'));
-    })
-</script>
--- resources/views/certification/users/list.blade.php
+++ resources/views/certification/users/list.blade.php
@@ -1,71 +1,100 @@
 <!DOCTYPE html>
 <html lang="ru">
-@include('certification.chunk.head')
-@include('certification.chunk.script')
-<body class="hold-transition sidebar-mini">
-<div class="wrapper">
-    <nav class="main-header navbar navbar-expand navbar-white navbar-light">
-        <ul class="navbar-nav">
-            <li class="nav-item">
-                <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
-            </li>
-            <li class="nav-item d-none d-sm-inline-block">
-                <a href="{{ route('home', ['member_id' => Auth::user()->member_id]) }}" class="nav-link">{{ __('Главная') }}</a>
-            </li>
-        </ul>
-    </nav>
-    @include('certification.chunk.sidebar')
-    <div class="content-wrapper">
-        <div class="content-header">
-            <div class="container-fluid">
-                <div class="row mb-2">
-                    <div class="col-sm-6">
-                        <h1 class="m-0">{{ __('Сотрудники') }}</h1>
-                    </div>
-                    <div class="col-sm-6">
-                        <ol class="breadcrumb float-sm-right">
-                            <li class="breadcrumb-item"><a href="{{ route('home', ['member_id' => Auth::user()->member_id]) }}">{{ __('Главная') }}</a></li>
-                            <li class="breadcrumb-item active">{{ __('Сотрудники') }}</li>
-                        </ol>
+    @include('certification.chunk.head')
+    @include('certification.chunk.script')
+    <body class="hold-transition sidebar-mini">
+        <div class="wrapper">
+            <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+                    </li>
+                    <li class="nav-item d-none d-sm-inline-block">
+                        <a href="{{ route('home', ['member_id' => $auth->member_id]) }}" class="nav-link">{{ __('Главная') }}</a>
+                    </li>
+                </ul>
+            </nav>
+            @include('certification.chunk.sidebar')
+            <div class="content-wrapper">
+                <div class="content-header">
+                    <div class="container-fluid">
+                        <div class="row mb-2">
+                            <div class="col-sm-6">
+                                <h1 class="m-0">{{ __('Сотрудники') }}</h1>
+                            </div>
+                            <div class="col-sm-6">
+                                <ol class="breadcrumb float-sm-right">
+                                    <li class="breadcrumb-item"><a href="{{ route('home', ['member_id' => $auth->member_id]) }}">{{ __('Главная') }}</a></li>
+                                    <li class="breadcrumb-item active">{{ __('Сотрудники') }}</li>
+                                </ol>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </div>
-
-        <div class="content">
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col-lg-12">
-                        <div class="card">
-                            <div class="card-body">
-                                <table class="table table-bordered">
-                                    <thead>
-                                        <tr>
-                                            <th class="text-center">#</th>
-                                            <th class="text-center">Bitrix ID</th>
-                                            <th class="text-center">{{ __('Статус') }}</th>
-                                            <th class="text-center">{{ __('Имя') }}</th>
-                                            <th class="text-center">{{ __('Email') }}</th>
-                                            <th class="text-center">{{ __('Поддержка') }}</th>
-                                            <th class="text-center">{{ __('Подразделения') }}</th>
-                                            <th class="text-center">{{ __('Язык') }}</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        @each('certification.users.list-table-element', $users, 'user')
-                                    </tbody>
-                                </table>
+                <div class="content">
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <table class="table table-bordered">
+                                            <thead>
+                                                <tr>
+                                                    <th class="text-center">#</th>
+                                                    <th class="text-center">Bitrix ID</th>
+                                                    <th class="text-center">{{ __('Статус') }}</th>
+                                                    <th class="text-center">{{ __('Имя') }}</th>
+                                                    <th class="text-center">{{ __('Email') }}</th>
+                                                    <th class="text-center">{{ __('Поддержка') }}</th>
+                                                    <th class="text-center">{{ __('Подразделения') }}</th>
+                                                    <th class="text-center">{{ __('Язык') }}</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                @each('certification.users.list-table-element', $users, 'user')
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                    <div class="card-footer">
+                                        {{ $users->links('pagination::bootstrap-4') }}
+                                    </div>
+                                </div>
                             </div>
-
                         </div>
                     </div>
                 </div>
             </div>
+            <aside class="control-sidebar control-sidebar-dark"></aside>
+            @include('certification.chunk.footer')
         </div>
-    </div>
-    <aside class="control-sidebar control-sidebar-dark">
-    </aside>
-    @include('certification.chunk.footer')
-</div>
-</body>
+        <script>
+            $("input[data-switch]").each(function(){
+                $(this).bootstrapSwitch('state');
+            })
+            function changeActiveStatus(userID, currentStatus) {
+                $.post("{{ route('user.updateActive', ['member_id' => $auth->member_id]) }}", {
+                    userID: userID,
+                    currentStatus: currentStatus
+                })
+                .done(function(data) {
+                    console.log(data);
+                })
+                .fail(function(data) {
+                    console.log(data);
+                })
+            }
+            function changeIsSupport(userID, isSupportStatus) {
+                $.post("{{ route('user.updateIsSupport', ['member_id' => $auth->member_id]) }}", {
+                    userID: userID,
+                    isSupportStatus: isSupportStatus
+                })
+                    .done(function(data) {
+                        console.log(data);
+                    })
+                    .fail(function(data) {
+                        console.log(data);
+                    })
+            }
+        </script>
+    </body>
 </html>
