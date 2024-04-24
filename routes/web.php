1800034325873e8ccca7289962548004c965961e
Evgeny Ignatyev <work@ignatyev.pro>
2024-04-24 17:22:21 +0200

    Обновление роутингов

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\DepartmentsController;
+use App\Http\Controllers\MainController;
+use App\Http\Controllers\QuestionsController;
 use App\Http\Controllers\Services\BitrixController;
-use App\Http\Middleware\AuthUserByMemberID;
+use App\Http\Controllers\UserController;
+use App\Http\Middleware\UserIsActiveMiddleware;
+use App\Http\Middleware\UserIsAdminMiddleware;
+use App\Http\Middleware\UserIsAdminOrSupportMiddleware;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Support\Facades\Route;
 
@@ -12,12 +18,26 @@ Route::get('/', function () {
 Route::prefix('services')->name('services.')->group(function () {
     Route::prefix('bitrix')->name('bitrix.')->group(function () {
         Route::prefix('certification')->name('certification.')->group(function () {
-            Route::post('/install', [BitrixController::class, 'install'])->withoutMiddleware([VerifyCsrfToken::class, AuthUserByMemberID::class])->name('install');
-            Route::get('/home/{member_id}', [BitrixController::class, 'home'])->middleware([AuthUserByMemberID::class])->name('home');
-            Route::get('/users/{member_id}', [BitrixController::class, 'getUsers'])->middleware([AuthUserByMemberID::class])->name('get.users');
-            Route::get('/departments/{member_id}', [BitrixController::class, 'getDepartments'])->middleware([AuthUserByMemberID::class])->name('get.departments');
-            Route::get('/tests/{member_id}', [BitrixController::class, 'getTests'])->middleware([AuthUserByMemberID::class])->name('get.tests');
-            Route::get('/certifications/{member_id}', [BitrixController::class, 'getCertifications'])->middleware([AuthUserByMemberID::class])->name('get.сertifications');
+            Route::post('/install', [BitrixController::class, 'install'])->withoutMiddleware([VerifyCsrfToken::class])->name('install');
         });
     });
+});
+
+Route::get('/home/{member_id}', [MainController::class, 'home'])->middleware([UserIsActiveMiddleware::class])->name('home');
+Route::get('/users/{member_id}', [UserController::class, 'getUsers'])->middleware([UserIsActiveMiddleware::class])->name('get.users');
+Route::get('/departments/{member_id}', [DepartmentsController::class, 'getDepartments'])->middleware([UserIsActiveMiddleware::class])->name('get.departments');
+Route::get('/tests/{member_id}', [MainController::class, 'getTests'])->middleware([UserIsActiveMiddleware::class])->name('get.tests');
+Route::get('/certifications/{member_id}', [MainController::class, 'getCertifications'])->middleware([UserIsActiveMiddleware::class])->name('get.сertifications');
+
+Route::prefix('user')->name('user.')->group(function () {
+    Route::post('/updateActive/{member_id}', [UserController::class, 'updateActive'])->middleware([UserIsActiveMiddleware::class, UserIsAdminOrSupportMiddleware::class])->withoutMiddleware([VerifyCsrfToken::class])->name('updateActive');
+    Route::post('/updateIsSupport/{member_id}', [UserController::class, 'updateIsSupport'])->middleware([UserIsActiveMiddleware::class, UserIsAdminMiddleware::class])->withoutMiddleware([VerifyCsrfToken::class])->name('updateIsSupport');
+});
+
+Route::prefix('departments')->name('departments.')->group(function () {
+    Route::post('/setManagers/{member_id}', [DepartmentsController::class, 'setManagers'])->middleware([UserIsActiveMiddleware::class, UserIsAdminOrSupportMiddleware::class])->withoutMiddleware([VerifyCsrfToken::class])->name('setManagers');
+});
+
+Route::prefix('questions')->name('questions.')->group(function () {
+    Route::get('/list/{member_id}', [QuestionsController::class, 'list'])->middleware([UserIsActiveMiddleware::class, UserIsAdminOrSupportMiddleware::class])->name('list');
 });
