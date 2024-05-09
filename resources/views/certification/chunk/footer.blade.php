f2e05d44ccabcd970136977c626f7497bbccbd45
Evgeny Ignatyev <work@ignatyev.pro>
2024-05-09 19:32:06 +0300

    Обновление чанков

--- resources/views/certification/chunk/footer.blade.php
+++ resources/views/certification/chunk/footer.blade.php
@@ -1,6 +1,5 @@
 <footer class="main-footer">
     <strong>Copyright &copy; 2024 All rights reserved.</strong>
-
     <div class="float-right d-none d-sm-inline-block">
         <b>Version</b> 0.0.1 (A)
     </div>
--- resources/views/certification/chunk/head.blade.php
+++ resources/views/certification/chunk/head.blade.php
@@ -2,7 +2,10 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Аттестация</title>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+{{--    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">--}}
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('plugins/fontawesome-free/css/all.min.css') }}">
     <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
     <link rel="stylesheet" href="{{ asset('css/adminlte.min.css') }}">
