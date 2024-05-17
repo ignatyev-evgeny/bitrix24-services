e7289e096e25c855fdcb2e9b1ea4e3547785816f
Evgeny Ignatyev <work@ignatyev.pro>
2024-05-18 01:27:36 +0300

    Обновление oAuth авторизации, теперь не требуется указывать адрес портала

--- app/Http/Controllers/oAuthController.php
+++ app/Http/Controllers/oAuthController.php
@@ -6,7 +6,8 @@ use Illuminate\Http\Request;
 
 class oAuthController extends Controller {
     public function bitrix(Request $request) {
-        $clearPortal = explode("/",explode("//", $request->portal)[1])[0];
+        //$clearPortal = explode("/",explode("//", $request->portal)[1])[0];
+        $clearPortal = "monkey.bitrix24.ru";
         $clientId = "local.6621555f3ead38.99934649";
         return redirect()->to("https://$clearPortal/oauth/authorize/?client_id=$clientId");
     }
--- resources/views/login/login.blade.php
+++ resources/views/login/login.blade.php
@@ -11,9 +11,6 @@
         </div>
         <div class="card-body">
             <form action="{{ route('oAuth.bitrix.login') }}" method="POST">
-                <div class="input-group mb-3">
-                    <input type="url" name="portal" required class="form-control text-center" placeholder="{{ __('Ссылка на портал Bitrix24') }}">
-                </div>
                 <div class="row">
                     <div class="col-12">
                         <button type="submit" class="btn btn-primary btn-block">{{ __('Войти через Bitrix24') }}</button>
